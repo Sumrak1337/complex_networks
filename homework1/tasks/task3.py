41e4e01d1fe8939a78c27a511f54f4c5513d1c5a
Sumrak1337 <sumrakpolyakov@mail.ru>
2022-12-06 22:32:02 +0300

    added task3.1 of homework1

--- homework1/tasks/task3.py
+++ homework1/tasks/task3.py
@@ -1,7 +1,14 @@
+import matplotlib.pyplot as plt
 import networkx as nx
+import matplotlib.colors as mcolors
+from matplotlib import cm
 
-from utils import AbstractTask
-from homework1.task_defaults import CLEAR_DATA_ROOT
+
+from utils import AbstractTask, get_logger
+from homework1.task_defaults import CLEAR_DATA_ROOT, RESULTS_ROOT
+
+colors = list(mcolors.TABLEAU_COLORS)
+log = get_logger(__name__)
 
 
 class Task3(AbstractTask):
@@ -15,4 +22,41 @@ class Task3(AbstractTask):
         lcc = max(nx.connected_components(self.G), key=len)
         subgraph = nx.subgraph(self.G, lcc)
 
-        # print(subgraph)
+        pos = nx.spring_layout(subgraph,
+                               seed=42)
+        cmap = plt.get_cmap('jet')
+
+        for centrality, c_tag in zip([nx.degree_centrality(subgraph),
+                                      nx.closeness_centrality(subgraph),
+                                      nx.betweenness_centrality(subgraph),
+                                      nx.eigenvector_centrality(subgraph)],
+                                     ['degree_cen',
+                                      'closeness_cen',
+                                      'betweenness_cen',
+                                      'eigenvector_cen']):
+            log.info(f'{c_tag}')
+            first10 = sorted(centrality.items(), key=lambda item: item[1], reverse=True)[:10]
+            log.info('The first 10 persons with the highest value:')
+            for k, v in first10:
+                print(f'{k:20} {v:.3f}')
+            print()
+            node_size = [v * 2e3 for v in centrality.values()]
+            norm = mcolors.Normalize(vmin=min(centrality.values()), vmax=max(centrality.values()))
+            scmp = cm.ScalarMappable(norm=norm, cmap=cmap)
+            color = [scmp.to_rgba(v, alpha=0.5) for v in centrality.values()]
+            self.plot_network(subgraph=subgraph,
+                              nodelist=nx.nodes(subgraph),
+                              pos=pos,
+                              color=color,
+                              title='default title',  # TODO: change
+                              tag=c_tag,
+                              save_root=RESULTS_ROOT,
+                              node_size=node_size)
+
+        """
+        TODO:
+        1. decay centrality
+        2. PageRank
+        3. Алгоритм HITS
+        """
+
