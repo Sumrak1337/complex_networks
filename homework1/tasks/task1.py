b7124969ca59a9596c576ee322e0803676989ee4
Sumrak1337 <sumrakpolyakov@mail.ru>
2022-12-26 15:27:07 +0300

    small additions

--- homework1/tasks/task1.py
+++ homework1/tasks/task1.py
@@ -1,5 +1,9 @@
+import random
+
 import networkx as nx
 import pandas as pd
+import numpy as np
+from tqdm import tqdm
 
 from utils import GraphData, get_logger
 from homework1.task_defaults import DATA_ROOT
@@ -32,7 +36,34 @@ class Task1:
             n_strong_cc = len(list(nx.strongly_connected_components(graph))) if graph_tag == GraphData.WEB_GRAPH.value else None
             weak_cc = max(nx.weakly_connected_components(graph), key=len) if graph_tag == GraphData.WEB_GRAPH.value else max(nx.connected_components(graph), key=len)
             strong_cc = max(nx.strongly_connected_components(graph), key=len) if graph_tag == GraphData.WEB_GRAPH.value else None
+            avg_degree = 2 * n_edges / n_nodes
+            greater_avg_degree = np.sum(np.array([True if value > avg_degree
+                                                  else False
+                                                  for value in dict(graph.degree()).values()]))
 
+            subgraph = nx.subgraph(graph, weak_cc)
+            n = min(500, len(weak_cc))
+            random.seed(42)
+            random_nodes = random.sample(list(nx.nodes(subgraph)), n)
+            all_paths = []
+            for i in tqdm(range(n - 1)):
+                for j in range(i + 1, n):
+                    s_path = nx.shortest_path_length(subgraph,
+                                                     source=random_nodes[i],
+                                                     target=random_nodes[j])
+                    all_paths.append(s_path)
+            all_paths = pd.Series(all_paths)
+
+            radius = np.inf
+            diameter = -np.inf
+            for node in tqdm(random_nodes):
+                e = nx.eccentricity(subgraph, node)
+                if e > diameter:
+                    diameter = e
+                if e < radius:
+                    radius = e
+
+            # Main features description for original graph
             log.info(f'# of nodes: {n_nodes}')
             log.info(f'# of edges: {n_edges}')
             log.info(f'Density: {n_edges / max_edges:.4f}')
@@ -41,6 +72,12 @@ class Task1:
             if graph_tag == GraphData.WEB_GRAPH.value:
                 log.info(f'Max strongly connected components: {n_strong_cc}')
                 log.info(f'Strongly percentile: {len(strong_cc) / n_nodes:.4f}')
+            log.info(f"# of nodes' degree greater than average: {greater_avg_degree}")
+
+            log.info('For the largest weakly connectivity component:')
+            log.info(f'Radius: {radius}')
+            log.info(f'Diameter: {diameter}')
+            log.info(f'P90: {all_paths.quantile(q=0.9)}')
 
     def get_graph(self, graph_tag):
         if graph_tag == GraphData.ASTROPH_GRAPH.value:
